6daf7f1939604d1cd3f865d2a72cad90312f8362
bwa2116 <151790222+bwa2116@users.noreply.github.com>
2024-04-17 23:55:34 +0200

    Update utils.py

--- ViT/utils.py
+++ ViT/utils.py
@@ -9,6 +9,31 @@ import torchvision.transforms as transforms
 from .model import ViTForClassfication
 
 
+import time
+
+class TimerError(Exception):
+    """A custom exception used to report errors in use of Timer class"""
+
+class Timer:
+    def __init__(self):
+        self._start_time = None
+
+    def start(self):
+        """Start a new timer"""
+        if self._start_time is not None:
+            raise TimerError(f"Timer is running. Use .stop() to stop it")
+
+        self._start_time = time.perf_counter()
+
+    def stop(self):
+        """Stop the timer, and report the elapsed time"""
+        if self._start_time is None:
+            raise TimerError(f"Timer is not running. Use .start() to start it")
+
+        elapsed_time = time.perf_counter() - self._start_time
+        self._start_time = None
+        print(f"Elapsed time: {elapsed_time:0.4f} seconds")
+      
 def save_experiment(experiment_name, config, model, train_losses, test_losses,
                     accuracies, time, base_dir="experiments"):
     outdir = os.path.join(base_dir, experiment_name)
