4b2b18696f493331c7c6f4f60f5b960ee79ef023
bwa2116 <151790222+bwa2116@users.noreply.github.com>
2024-04-28 16:55:11 +0200

    Update loader.py

--- datasets/loader.py
+++ datasets/loader.py
@@ -249,11 +249,11 @@ def imagenet200(img_size=(64, 64), batch_size=4, num_workers=2):
             transforms.ToTensor(),
             transforms.Resize(img_size), 
             transforms.RandomHorizontalFlip(p=0.5),
-            transforms.RandomResizedCrop(
-                img_size,
-                scale=(0.8, 1.0),
-                ratio=(0.75, 1.3333333333333333),
-                interpolation=2),
+            # transforms.RandomResizedCrop(
+            #     img_size,
+            #     scale=(0.8, 1.0),
+            #     ratio=(0.75, 1.3333333333333333),
+            #     interpolation=2),
             transforms.Normalize(
                 (0.4914, 0.4822, 0.4465),
                 (0.2023, 0.1994, 0.2010)
